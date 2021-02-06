301346c322dfb7002407613ad59e1cf0afb27c17
rich <r.k.scott@outlook.com>
2021-02-06 02:57:34 +0000

    added interfaces for pet clinic services. closes #5

---
+++ pet-clinic-data/src/main/java/guru/springframework/services/VetService.java
@@ -0,0 +1,12 @@
+package guru.springframework.services;
+
+
+
+import guru.springframework.sfgpetclinic.model.Pet;
+import java.util.Set;
+
+public interface VetService {
+    Pet findById(Long id);
+    Pet save(Pet pet);
+    Set<Pet> findAll();
+}
