3477ab8629c45555536d1b39fda2c1b9f4ed4e99
rich <r.k.scott@outlook.com>
2021-02-06 23:12:08 +0000

    implemented map based services. closes #6

---
+++ pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/map/AbstractMapService.java
@@ -0,0 +1,28 @@
+package guru.springframework.sfgpetclinic.map;
+
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Map;
+import java.util.Set;
+
+public abstract class AbstractMapService<T, ID> {
+
+    protected Map<ID, T> map = new HashMap<>();
+    protected Set<T> findAll(){
+        return new HashSet<>(map.values());
+    };
+    protected T findById(ID id) {
+        return map.get(id);
+    }
+
+   protected T save(ID id ,T object) {
+        map.put(id, object);
+        return object;
+    }
+    protected void deleteById(ID id) {
+        map.remove(id);
+    }
+    protected void delete(T object) {
+        map.entrySet().removeIf(entry -> entry.getValue().equals(object));
+    }
+}
---
+++ pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/services/PetServiceMap.java
@@ -0,0 +1,33 @@
+package guru.springframework.sfgpetclinic.services;
+
+import guru.springframework.sfgpetclinic.map.AbstractMapService;
+import guru.springframework.sfgpetclinic.model.Pet;
+
+import java.util.Set;
+
+public class PetServiceMap extends AbstractMapService<Pet, Long> implements CrudService<Pet, Long> {
+    @Override
+    public Set<Pet> findAll() {
+        return super.findAll();
+    }
+
+    @Override
+    public Pet findById(Long id) {
+        return super.findById(id);
+    }
+
+    @Override
+    public Pet save(Pet object) {
+        return super.save(object.getId(), object);
+    }
+
+    @Override
+    public void delete(Pet object) {
+        super.delete(object);
+    }
+
+    @Override
+    public void deleteById(Long id) {
+        super.deleteById(id);
+    }
+}
