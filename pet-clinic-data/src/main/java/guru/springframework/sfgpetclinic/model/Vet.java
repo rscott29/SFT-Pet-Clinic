d3c1c7bb138087df87802ba3270315e3993ba198
rich <r.k.scott@outlook.com>
2021-02-07 21:05:54 +0000

    create specialty object updated vet

--- pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/model/Vet.java
+++ pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/model/Vet.java
@@ -1,5 +1,15 @@
 package guru.springframework.sfgpetclinic.model;
 
-public class Vet extends Person {
+import java.util.Set;
 
+public class Vet extends Person {
+    private Set<Speciality> specialities;
+
+    public Set<Speciality> getSpeciality() {
+        return specialities;
+    }
+
+    public void setSpeciality(Set<Speciality> specialities) {
+        this.specialities = specialities;
+    }
 }
